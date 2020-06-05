3e420e45b58b564167e4427aa348bf4278285424
Prerna <prerna1998.ps@gmail.com>
2020-06-05 09:58:32 +0530

    Rendering data in a UD Component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Navbar,NavbarBrand} from 'reactstrap';
 import logo from './logo.svg';
 import './App.css';
+import Menu from './components/MenuComponent';
 
 function App() {
   return (
@@ -10,7 +11,8 @@ function App() {
         <div className="container">
         <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
         </div>
-      </Navbar>   
+      </Navbar>  
+      <Menu/>
     </div>
   );
 }
