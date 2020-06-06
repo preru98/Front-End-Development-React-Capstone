f54d2ae35013e4153e02178a0db8102b70d2ecb8
Prerna <prerna1998.ps@gmail.com>
2020-06-06 20:36:09 +0530

    Rendering and UI Logic : Presentational Component

---
+++ src/components/DishDetailComponent.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+
+class DishDetail extends Component{
+    constructor(props){
+        super(props);
+    }
+    renderDish(dish) {
+        if (dish != null){
+            return(
+                <Card>
+                    <CardImg top src={dish.image} alt={dish.name} />
+                    <CardBody>
+                      <CardTitle>{dish.name}</CardTitle>
+                      <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            );
+        }
+        else{
+            return(
+                <div></div>
+            );
+        }
+    }
+    renderComments(dish) {
+        if (dish != null){
+            const COMMENTS=dish.comments.map((COMMENT)=>{
+                return(
+                    <ul className="list-unstyled">
+                        <li key={COMMENT.id}>{COMMENT.comment}<br/>by {[COMMENT.author,COMMENT.date.slice(0,10)].join(' on ')}</li>
+                    </ul>
+                );
+            });
+            return(
+                <div>
+                    <h4>Comments</h4>
+                    <div>{COMMENTS}</div>
+                </div>
+            );
+        }
+        else{
+            return(
+                <div></div>
+            );
+        }
+    }
+    render(){
+        return(
+            <div className="row">
+                  <div  className="col-12 col-md-5 m-1">
+                    {this.renderDish(this.props.selectedDish)}
+                  </div>
+                  <div  className="col-12 col-md-5 m-1">
+                    {this.renderComments(this.props.selectedDish)}
+                  </div>
+            </div>
+        );
+    }
+}
+
+export default DishDetail;
