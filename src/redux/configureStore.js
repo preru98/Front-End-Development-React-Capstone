2caefc203d32c5af1e5719761e15153ae1063854
Prerna <prerna1998.ps@gmail.com>
2020-06-17 16:09:37 +0530

    Redux | Splitting & Combining Reducers | Organizing Applicaton

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,7 +1,18 @@
-import { createStore } from 'redux';
-import { initialState, Reducer } from './reducer';
+import { createStore, combineReducers } from 'redux';
+import { Dishes } from './dishes';
+import { Promotions } from './promotions';
+import { Comments } from './comments';
+import { Leaders } from './leaders';
 
 export const ConfigureStore=()=>{
-    const store=createStore(Reducer, initialState);
+    const store=createStore(
+        combineReducers({
+            //This is our global state here :)
+            dishes : Dishes,
+            promotions : Promotions,
+            leaders : Leaders,
+            comments : Comments
+        })
+    );
     return store;
 }
